55e3efad5e6626f356c92f2c799cc391c3b28779
hgmk <hal20058@byui.edu>
2020-11-02 18:26:04 +0000

    for readability

--- javascript/discount.js
+++ javascript/discount.js
@@ -4,16 +4,16 @@ function discountPrice() {
     let subTotal = parseFloat(document.getElementById("subTotal").value);
     let now = new Date();
     let dayOfWeek = now.getDay();
-    let minusPerc = 0.1;
+    let discountPercentage = 0.1;
     let saleTax = 0.06;
-    let disPerc;
-    let taxPerc;
+    let discountAmount;
+    let taxAmount;
     let total;
     //PROCESSING
     if (((dayOfWeek == 2) || (dayOfWeek == 3)) && (subTotal > 50)) {
-        disPerc =  subTotal - subTotal * minusPerc
-        taxPerc = disPerc * saleTax
-        total = disPerc + taxPerc
+        discountAmount =  subTotal - subTotal * discountPercentage
+        taxAmount = discountAmount * saleTax
+        total = discountAmount + taxAmount
         let digits = 2;
         let multiplier = Math.pow(10, digits);
         total = Math.round(total * multiplier) / multiplier;
