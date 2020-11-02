a145fa632621f506bae97b46934b9e309ac4930c
hgmk <hal20058@byui.edu>
2020-11-02 15:36:38 +0000

    discount

---
+++ javascript/discount.js
@@ -0,0 +1,29 @@
+
+function discountPrice() {
+    //INPUT
+    let subTotal = parseFloat(document.getElementById("subTotal").value);
+    let now = new Date();
+    let dayOfWeek = now.getDay();
+    let minusPerc = 0.1;
+    let saleTax = 0.06;
+    let disPerc;
+    let taxPerc;
+    let total;
+    //PROCESSING
+    if (((dayOfWeek == 2) || (dayOfWeek == 3)) && (subTotal > 50)) {
+        disPerc =  subTotal - subTotal * minusPerc
+        taxPerc = disPerc * saleTax
+        total = disPerc + taxPerc
+        let digits = 2;
+        let multiplier = Math.pow(10, digits);
+        total = Math.round(total * multiplier) / multiplier;
+    }
+    else {
+        total = subTotal * saleTax + subTotal
+        let digits = 2;
+        let multiplier = Math.pow(10, digits);
+        total = Math.round(total * multiplier) / multiplier;
+    }
+    //OUTPUT
+    document.getElementById("output").innerHTML = total;
+}
